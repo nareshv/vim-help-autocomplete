b82894ddc90c28885fdc22d1a644d0c7b5109168
nareshv <vnareshkumar@gmail.com>
2014-01-30 17:50:39 +0530

    fixed some bugs in identifying sections

--- parse-vim-options.pl
+++ parse-vim-options.pl
@@ -6,7 +6,7 @@ use Data::Dumper;
 use DBI;
 
 my $dbh = DBI->connect('dbi:mysql:vim_db;host=127.0.0.1','root') or die "$DBI::errstr\n";
-my $sql = "CREATE TABLE IF NOT EXISTS vim_options (id integer auto_increment primary key, option varchar(200) unique, short_desc text DEFAULT NULL, data_type varchar(64) default null, data_type_values varchar(200) default null, long_desc text DEFAULT NULL, is_global tinyint(1) DEFAULT NULL, local_to_window tinyint(1) DEFAULT NULL, local_to_buffer tinyint(1) DEFAULT NULL, not_in_vi tinyint(1) DEFAULT NULL, req_feature varchar(200) DEFAULT NULL, added_at datetime, updated_at datetime) charset=latin1";
+my $sql = "CREATE TABLE IF NOT EXISTS vim_options (id integer auto_increment primary key, option varchar(200) unique, short_desc text DEFAULT NULL, data_type varchar(64) default null, data_type_values varchar(200) default null, long_desc text DEFAULT NULL, is_global tinyint(1) DEFAULT NULL, local_to_window tinyint(1) DEFAULT NULL, local_to_buffer tinyint(1) DEFAULT NULL, not_in_vi tinyint(1) DEFAULT NULL, req_feature varchar(200) DEFAULT NULL, added_at datetime, updated_at datetime, index (`data_type`), index (`req_feature`)) charset=latin1";
 $dbh->do($sql) || die "$!";
 my $sth = $dbh->prepare( "INSERT into vim_options set option = ?, short_desc = ?, data_type = ?, data_type_values = ?, long_desc = ?, is_global = ?, local_to_window = ?, local_to_buffer = ?, not_in_vi = ?, req_feature = ?, added_at = now() on duplicate key update updated_at = now()");
 
@@ -27,7 +27,8 @@ my $shdesc_ended = 0;
 
 while(<STDIN>) {
 	$line = $_;
-	if ($line =~ /^('.*?)\t+([a-z].*)$/) {
+	#if ($line =~ /^('.*?)\t+([a-z].*)$/) {
+	if ($line =~ /^('.*?')\s+(string|boolean|number)\s+(.*)/i) {
 		$all_lc = 1;
 		$optdata= "";
 		$latest = $1;
@@ -55,20 +56,20 @@ while(<STDIN>) {
 		# parse the options dataset
 		if (defined $latest and not defined $tmpbuf{$latest}) {
 			# FIXME: Write this only once for each option
-			$options{$latest}[1] .= "===========================\n";
+			#$options{$latest}[1] .= "===========================\n";
 			$optdata =~ s/^\s+//;
 			$optdata =~ s/[\n\t]/ /g;
 			$optdata =~ s/\s+/ /g;
 			$optdata =~ s/\{/\n{/g;
-			$options{$latest}[1] .= $optdata."\n";
-			$options{$latest}[1] .= "===========================\n";
+			#	$options{$latest}[1] .= $optdata."\n";
+			#$options{$latest}[1] .= "===========================\n";
 			$tmpbuf{$latest} = 1;
 		}
 		# Parse the optdata which has these options
 		my @options = split(/\n/, $optdata);
 		my %features = ();
 		foreach my $aopt (@options) {
-			if ($aopt =~ /^global/) {
+			if ($aopt =~ /^.*?global/) {
 				$options{$latest}[2] = 1;
 			}
 			if ($aopt =~ /^.*?local to window/i) {
@@ -80,10 +81,17 @@ while(<STDIN>) {
 			if ($aopt =~ /^.*?not in vi/i) {
 				$options{$latest}[5] = 1;
 			}
+			if ($aopt =~ /^.*?not in all versions of vi/i) {
+				$options{$latest}[5] = 2;
+			}
 			if ($aopt =~ /compiled/) {
-				while($aopt =~ m/\|(.*?)\|/ig) {
+				#if ($latest =~ /previewwindow/) {
+				#	print $aopt;
+				#	exit;
+				#}
+				while($aopt =~ /\|(.*?)\|/g) {
 					$features{$1} = 1;
-				}
+			       	}
 				$options{$latest}[6] = join(',', keys %features);
 			}
 		}
@@ -99,8 +107,8 @@ foreach my $opt (keys %options) {
 	$s =~ s/\n/ /g;
 	# Generate the datatype and its values
 	my ($datatype, $datatype_values) = ($s =~ /^(.*?)\((.*?)\)/);
-	$datatype =~ s/\s+//;
-	$datatype_values =~ s/\t+/ /g;
+	$datatype =~ s/\s+// if defined $datatype;
+	$datatype_values =~ s/\t+/ /g if defined $datatype_values;
 	# Generate the CSV of option and its types
 	my $opt_csv = $opt;
 	$opt_csv =~ s/'//g;
@@ -109,20 +117,32 @@ foreach my $opt (keys %options) {
 	$opt_csv =~ s/,+/,/g;
 	# Sanitize the help data
 	my $fulltext = $options{$opt}[1];
-	$sth->execute($opt_csv, $s, $datatype, $datatype_values, $fulltext, $options{$opt}[2], $options{$opt}[3], $options{$opt}[4], $options{$opt}[5], $options{$opt}[6]) || die $!;
-	print("========= $opt =========");
-	printf("Option           : %s\n", $opt);
-	printf("Short            : %s\n", $s);
-	printf("Is Global        : %s\n", $options{$opt}[2]);
-	printf("Is Local File    : %s\n", $options{$opt}[3]);
-	printf("Is Local Buffer  : %s\n", $options{$opt}[3]);
-	printf("Not In Vi        : %s\n", $options{$opt}[4]);
-	printf("Feature          : %s\n", $options{$opt}[5]);
-	print $fulltext;
-	print("");
+	# Generate short description from the long text as $s is really default values
+	my $short_text = $fulltext;
+	$short_text =~ s/\s+/ /g;
+        if ($short_text =~ m/^(\s*.*?\. )/) {
+		$short_text = $1;
+	}
+	# Remove the next section heading from full text
+	$fulltext =~ s/\t+\*'\w+'\* \*'\w+'\*//g;
+	$sth->execute($opt_csv, $short_text, $datatype, $datatype_values, $fulltext, $options{$opt}[2], $options{$opt}[3], $options{$opt}[4], $options{$opt}[5], $options{$opt}[6]) || die $!;
+	if (defined $ENV{ON_SCREEN}) {
+		print("========= $opt =========");
+		printf("Option           : %s\n", $opt);
+		printf("Short            : %s\n", $s);
+		printf("Is Global        : %s\n", $options{$opt}[2]);
+		printf("Is Local File    : %s\n", $options{$opt}[3]);
+		printf("Is Local Buffer  : %s\n", $options{$opt}[3]);
+		printf("Not In Vi        : %s\n", $options{$opt}[4]);
+		printf("Feature          : %s\n", $options{$opt}[5]);
+		print $fulltext;
+		print("");
+	}
 	#printf("%-16s\t%s\n", $opt, $options{$opt}[0]);
 	#print $options{$opt}[1];
+	$c++;
 }
 $sth->finish;
 $dbh->disconnect;
+print "Executed $c queries.";
 exit;
